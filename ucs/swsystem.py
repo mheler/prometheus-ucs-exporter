512f2364e7e41926d40b0888c6b2a43e6781b037
mheler <matthew.heler@hotmail.com>
2023-05-06 21:15:13 -0500

    Update swsystem.py

--- ucs/swsystem.py
+++ ucs/swsystem.py
@@ -9,8 +9,8 @@ from . import utils as u
 cpu_load = Gauge('ucs_cpu_load', 'CPU load', ('domain', 'switch'))
 mem_available = Gauge('ucs_mem_available', 'Memory available', ('domain', 'switch'))
 mem_cached = Gauge('ucs_mem_cached', 'Memory cached', ('domain', 'switch'))
-kernel_mem_free = Gauge('ucs_kernel_mem_free', 'Kernel memory free', ('domain', 'switch'))
-kernel_mem_total = Gauge('ucs_kernel_mem_total', 'Kernel memory total', ('domain', 'switch'))
+# kernel_mem_free = Gauge('ucs_kernel_mem_free', 'Kernel memory free', ('domain', 'switch'))
+# kernel_mem_total = Gauge('ucs_kernel_mem_total', 'Kernel memory total', ('domain', 'switch'))
 
 class SwSystem:
     def __init__(self, domain):
@@ -22,5 +22,5 @@ class SwSystem:
             cpu_load.labels(self.domain, switch).set(float(item.load))
             mem_available.labels(self.domain, switch).set(int(item.mem_available))
             mem_cached.labels(self.domain, switch).set(int(item.mem_cached))
-            kernel_mem_total.labels(self.domain, switch).set(int(item.kernel_mem_total))
-            kernel_mem_free.labels(self.domain, switch).set(int(item.kernel_mem_free))
+            # kernel_mem_total.labels(self.domain, switch).set(int(item.kernel_mem_total))
+            # kernel_mem_free.labels(self.domain, switch).set(int(item.kernel_mem_free))
