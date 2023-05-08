4da54ac90bef26178d4a415b9884976fd1f86fe9
mheler <matthew.heler@hotmail.com>
2023-05-08 10:31:11 -0500

    Update faults.py

--- ucs/faults.py
+++ ucs/faults.py
@@ -6,7 +6,14 @@
 from collections import defaultdict
 from prometheus_client import Gauge
 
-ucs_faults_total = Gauge('ucs_faults_total', 'Faults', ['domain', 'type', 'description', 'dn', 'severity', 'code', 'cause'])
+ucs_faults_total = Gauge('ucs_faults_total', 'Faults', ['domain', 'type', 'description', 'dn', 'code', 'cause'])
+
+# Cleared 	= 0
+# Informational = 1
+# Minor 	= 2
+# Warning	= 3
+# Major 	= 4
+# Critical	= 5
 
 class Faults:
     def __init__(self, domain):
@@ -17,7 +24,17 @@ class Faults:
         metrics = defaultdict(list)
 
         for fault in faults:
-            active = 1
-            labels = {'domain': self.domain, 'type': fault.type, 'description': fault.descr, 'dn': fault.dn, 'severity': fault.severity, 'code': fault.code, 'cause': fault.cause }
-            ucs_faults_total.labels(**labels).set(active)
+            state = 0
+            if fault.severity == 'info':
+            	state = 1
+            if fault.severity == 'minor':
+            	state = 2
+            if fault.severity == 'warning':
+            	state = 3
+            if fault.severity == 'major':
+            	state = 4
+            if fault.severity == 'critical':
+            	state = 5
+            labels = {'domain': self.domain, 'type': fault.type, 'description': fault.descr, 'dn': fault.dn, 'code': fault.code, 'cause': fault.cause }
+            ucs_faults_total.labels(**labels).set(state)
         return
